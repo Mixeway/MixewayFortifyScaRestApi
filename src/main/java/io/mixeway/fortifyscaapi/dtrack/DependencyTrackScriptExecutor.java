a867481e16709181eb14736a0db634673f10762a
siewrgrz <grzegorz.siewruk@orange.com>
2020-03-17 14:08:26 +0100

    Remove sensitive logs

--- src/main/java/io/mixeway/fortifyscaapi/dtrack/DependencyTrackScriptExecutor.java
+++ src/main/java/io/mixeway/fortifyscaapi/dtrack/DependencyTrackScriptExecutor.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Arrays;
 
 public class DependencyTrackScriptExecutor {
     private Logger logger = LoggerFactory.getLogger(DependencyTrackScriptExecutor.class);
@@ -16,9 +15,6 @@ public class DependencyTrackScriptExecutor {
 
         ProcessBuilder builder = new ProcessBuilder();
         builder.command("/bin/sh", script,project.getdTrackUuid(), createScanRequest.getdTrackUrl(), createScanRequest.getdTrackToken());
-        logger.info("Running {} inside {}",String.join(" ",
-                Arrays.asList("sh", "-c", script,project.getdTrackUuid(), createScanRequest.getdTrackUrl(), createScanRequest.getdTrackToken())),
-                path.toFile());
         builder.directory(path.toFile());
         Process process = builder.start();
     }

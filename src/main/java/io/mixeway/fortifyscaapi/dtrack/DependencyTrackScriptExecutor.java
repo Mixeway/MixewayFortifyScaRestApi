ae2f5ec77c8f1a8c78b932b877c70aa5c2375345
siewrgrz <grzegorz.siewruk@orange.com>
2020-03-17 13:18:03 +0100

    Debugging.

--- src/main/java/io/mixeway/fortifyscaapi/dtrack/DependencyTrackScriptExecutor.java
+++ src/main/java/io/mixeway/fortifyscaapi/dtrack/DependencyTrackScriptExecutor.java
@@ -1,17 +1,13 @@
 package io.mixeway.fortifyscaapi.dtrack;
 
+import com.sun.deploy.util.ArrayUtil;
 import io.mixeway.fortifyscaapi.pojo.CreateScanRequest;
 import io.mixeway.fortifyscaapi.pojo.Project;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class DependencyTrackScriptExecutor {
     private Logger logger = LoggerFactory.getLogger(DependencyTrackScriptExecutor.class);
@@ -21,6 +17,9 @@ public class DependencyTrackScriptExecutor {
 
         ProcessBuilder builder = new ProcessBuilder();
         builder.command("sh", "-c", script,project.getdTrackUuid(), createScanRequest.getdTrackUrl(), createScanRequest.getdTrackUrl());
+        logger.info("Running {} inside {}",String.join(" ",
+                Arrays.asList("sh", "-c", script,project.getdTrackUuid(), createScanRequest.getdTrackUrl(), createScanRequest.getdTrackUrl())),
+                path.toFile());
         builder.directory(path.toFile());
         Process process = builder.start();
     }
